5a38bffb0db9e51d88dd96fe8065583f9549edc1
Eklegend <eklegend02@gmail.com>
2023-11-19 21:28:16 +0100

    19/11/2023 21.28

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import os
 
+import cv2
+
+cap = cv2.VideoCapture(0)
+# here we are using default camera sizing, either 1280*720, 640*480 or 320*240 or 160*120
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/fullscope.png')
+
+# import the mode images into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
+print (len(imgModeList))
+
+
+while True:
+    success, img = cap.read()
+
+    #  imgBackground[y_offset:y_offset + region_height, x_offset:x_offset + region_width] = img
+    imgBackground[137:137 + 480, 61:61 + 640] = img
+    imgBackground[0:0 + 720, 757:757 + 523] = imgModeList[0]
+
+    cv2.imshow("Face Attendance", imgBackground)
+
+    # Exit on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
