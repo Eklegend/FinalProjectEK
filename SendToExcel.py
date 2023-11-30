52e8d4c5cd0260e5c77f8ac04c2860bdc6caf7a2
Eklegend <124159257+Eklegend@users.noreply.github.com>
2023-11-30 04:34:13 +0100

    30/11/2023 04.33

--- SendToExcel.py
+++ SendToExcel.py
@@ -3,7 +3,6 @@ from firebase_admin import credentials
 from firebase_admin import db
 import pandas as pd
 
-
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate("serviceAccountKey.json")
 
@@ -18,13 +17,27 @@ ref = db.reference('Students')
 # Retrieve data from Firebase
 firebase_data = ref.get()
 
-# Transform Firebase data into a Pandas DataFrame
-# For example, assuming you have fetched a dictionary of data
-# Convert it into a DataFrame for easy manipulation
-df = pd.DataFrame.from_dict(firebase_data)
 
-# Write data to Excel file using Pandas
-excel_file_path = 'StudentData.xlsx'
-df.to_excel(excel_file_path, index=False)
+def update_excel_file(event):
+    firebase_data = ref.get()
+    # Extract Keys and values
+    keys = []
+    values = []
+    for key, value in firebase_data.items():
+        keys.append(key)
+        values.append(value)
 
-print(f"Data has been exported to '{excel_file_path}'")
+    # Transform Firebase data into a Pandas DataFrame
+    # For example, assuming you have fetched a dictionary of data
+    # Convert it into a DataFrame for easy manipulation
+    df = pd.DataFrame({'Keys': keys, 'Values': values})
+
+    # Write data to Excel file using Pandas
+    excel_file_path = 'StudentData.xlsx'
+    df.to_excel(excel_file_path, index=False)
+
+    print(f"Data has been exported to '{excel_file_path}'")
+
+
+# this listens to updates and updates the excel file
+ref.listen(update_excel_file)
