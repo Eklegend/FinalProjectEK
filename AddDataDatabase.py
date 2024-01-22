038b7048823555ec22dec409eeec077eaf3cbaf2
Eklegend <124159257+Eklegend@users.noreply.github.com>
2024-01-22 02:50:38 +0100

    30/12/2023 01.51

--- AddDataDatabase.py
+++ AddDataDatabase.py
@@ -13,7 +13,7 @@ firebase_admin.initialize_app(cred, {
 # This is the reference path of the database that will create a student directory that will have student id
 ref = db.reference('Courses')
 
-data = {
+computer_science = {
     "121212":
         {
             "Name": "Hariharan Elangovan",
@@ -49,46 +49,7 @@ data = {
         }
 }
 
-data2 = {
-    "121212":
-        {
-            "Name": "Hariharan Elangovan",
-            "Major": "Informatics",
-            "Enrolling_Year": "2020",
-            "Total_attendance": 0,
-            "Standing": 4.6,  # G for good
-            "Year": 4,
-            "Last_attendance_time": "2023-01-01 01:01:01"
-
-        },
-    "131313":
-        {
-            "Name": "Alexandra Daddario",
-            "Major": "Data Finance",
-            "Enrolling_Year": "2021",
-            "Total_attendance": 0,
-            "Standing": 5.0,  # G for good
-            "Year": 5,
-            "Last_attendance_time": "2023-01-01 01:01:01"
-
-        },
-    "141414":
-        {
-            "Name": "Tony Stark",
-            "Major": "Computer Science",
-            "Enrolling_Year": "2023",
-            "Total_attendance": 0,
-            "Standing": 3.0,  # G for good
-            "Year": 1,
-            "Last_attendance_time": "2023-01-01 01:01:01"
-
-        }
-
-
-
-}
-
-data3 = {
+informatics = {
     "121212":
         {
             "Name": "Hariharan Elangovan",
@@ -126,9 +87,48 @@ data3 = {
 
 
 }
-ref.child('Computer Science').set(data)
-ref.child('Informatics').set(data2)
-ref.child('Computer Architecture').set(data3)
+
+comp_architect = {
+    "121212":
+        {
+            "Name": "Hariharan Elangovan",
+            "Major": "Informatics",
+            "Enrolling_Year": "2020",
+            "Total_attendance": 0,
+            "Standing": 4.6,  # G for good
+            "Year": 4,
+            "Last_attendance_time": "2023-01-01 01:01:01"
+
+        },
+    "131313":
+        {
+            "Name": "Alexandra Daddario",
+            "Major": "Data Finance",
+            "Enrolling_Year": "2021",
+            "Total_attendance": 0,
+            "Standing": 5.0,  # G for good
+            "Year": 5,
+            "Last_attendance_time": "2023-01-01 01:01:01"
+
+        },
+    "141414":
+        {
+            "Name": "Tony Stark",
+            "Major": "Computer Science",
+            "Enrolling_Year": "2023",
+            "Total_attendance": 0,
+            "Standing": 3.0,  # G for good
+            "Year": 1,
+            "Last_attendance_time": "2023-01-01 01:01:01"
+
+        }
+
+
+
+}
+ref.child('Computer Science').set(computer_science)
+ref.child('Informatics').set(informatics)
+ref.child('Computer Architecture').set(comp_architect)
 
 
 
